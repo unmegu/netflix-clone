017e262093797986a94f5f51f0c538169a75dc2b
unmegu <unnomgm@gmail.com>
2024-09-26 09:48:37 +0900

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
+import { requests } from "./request";
+import { Row } from "./components/Row";
 
+function App() {
   return (
-    <>  
-    </>
-  )
+   // 追加箇所
+    <div className="App">
+     <Row fetchUrl={requests.fetchNetflixOriginals} />
+    </div>
+  );
 }
 
-export default App
+export default App;
